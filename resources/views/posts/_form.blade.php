838252f3414bcaa759f13ce5405a61cb9b548899
decar08 <deart.94@gmail.com>
2023-09-07 20:44:26 -0500

    Se realiza modificación en la función create desde el controlador, se crea formulario para funciones de creación y actualización

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,1 +1,13 @@
-Formulario
+@csrf
+
+<label class="uppercaase text-white-700 text-xs">Título</label>
+<input type="text" name="title" class="rounded border-black-200 w-full mb-4" value="{{ $posts->title }}">
+
+<label class="uppercaase text-white-700 text-xs">Contenido</label>
+<textarea name="body" rows="5" class="rounded border-black-200 w-full mb-4">{{ $posts->body }}</textarea>
+
+<div class="flex justify-between items-center">
+    <a href="{{ route('posts.index') }}" class="text-indigo-600">Volver</a>
+
+    <input type="submit" value="Enviar" class="bg-red-800 text-white rounded px-4 py-2">
+</div>
