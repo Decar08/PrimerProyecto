58b8eaa8820e52e6ced838abb41f8ff25a0414db
decar08 <deart.94@gmail.com>
2023-09-07 19:22:41 -0500

    Se crea función eliminar (destroy) y se añade botón eliminar desde la vista index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,4 +12,10 @@ class PostController extends Controller
             'posts' => Post::latest() -> paginate()
         ]);
     }
+
+    public function destroy(Post $post){
+        
+        $post -> delete();
+        return back();
+    }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,7 +19,19 @@
                             <td class="px-6 py-4">
                                 <a href="" class="text-indigo-600">Editar</a>
                             </td>
-                            <td class="px-6 py-4">Eliminar</td>
+                            <td class="px-6 py-4">
+                                <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <input 
+                                        type="submit" 
+                                        value="Eliminar"
+                                        class="bg-red-800 text-white rounded px-4 py-2"
+                                        onclick="return confirm('¿Desea eliminar?')"
+                                    >
+                                </form>
+                            </td>
                         </tr>
 
                         @endforeach
