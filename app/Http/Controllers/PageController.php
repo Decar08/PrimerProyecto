dce9bf6722824586d6fb3182405f13f8a26805cc
decar08 <deart.94@gmail.com>
2023-09-17 17:27:28 -0500

    Se realizan configuraciones en las rutas web, cambios en el pageController y en la vista home

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,27 +9,30 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function home(){
-        return view('home');
-    }
-
-    public function blog(){
-        //consulta a base de datos mediante eloquent
-        //$posts = Post::get();
-
-        //trae el primer elemento
-        // $post = Post::first();
-
-        //Busca por ID
-        //$post = Post::find(8);
-
-        //Muestra de forma grafica
-        //dd($post);
 
         $posts = Post::latest()->paginate();
 
-        //Apunta a la vista blog y muestra la información en un array
-        return view ('blog', ['posts' => $posts]);
+        return view('home', ['posts' => $posts]);
     }
+
+    // public function blog(){
+    //     //consulta a base de datos mediante eloquent
+    //     //$posts = Post::get();
+
+    //     //trae el primer elemento
+    //     // $post = Post::first();
+
+    //     //Busca por ID
+    //     //$post = Post::find(8);
+
+    //     //Muestra de forma grafica
+    //     //dd($post);
+
+    //     $posts = Post::latest()->paginate();
+
+    //     //Apunta a la vista blog y muestra la información en un array
+    //     return view ('blog', ['posts' => $posts]);
+    // }
 
     public function post(Post $post){
 
